ab999c8c2fb7578e9633dfaa4355e8b467af1689
gopoma <gordono@unsa.edu.pe>
2023-08-22 16:52:00 -0500

    implement product's dtos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,62 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsEnum,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export enum Size {
+  XS = 'XS',
+  S = 'S',
+  M = 'M',
+  L = 'L',
+  XL = 'XL',
+  XXL = 'XXL',
+  XXXL = 'XXXL',
+}
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(1)
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsEnum(Size, { each: true })
+  @IsArray()
+  sizes: Size[];
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  tags?: string[];
+
+  @IsString({ each: true })
+  @IsArray()
+  @IsOptional()
+  images?: string[];
+}
